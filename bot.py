4ab7d967da194e03c66da0885fcc3fa25eb20323
reza0310 <70545529+reza0310@users.noreply.github.com>
2022-02-04 16:20:05 +0100

    Création du fichier de base
    Création du fichier "bot.py"

---
+++ bot.py
@@ -0,0 +1,19 @@
+# Importation de quelques modules nécessaires pour ajouter le dossier parent au chemin de recherche de l'import pour importer l'objet du prof à hériter
+import os, sys
+# Récupération du path (Chemin de fichier) du script actuel (bot.py):
+# -La variable __file__ est un objet désignant le fichier actuel
+# -os.path.dirname() permet d'en extraire le path
+
+# Exemple de path: D:/GitHub/beagleboys/main.py
+path = os.path.dirname(__file__)
+# Ajout du path du dossier parent à la liste de recher d'importe:
+# -os.sep est une variable contenant le séparateur de path de notre système d'exploitation (/ pour Linux, \ pour windows, ...)
+# -path[:path.rfind(os.sep)] permet d'extraire une sous-chaîne en l'occurence la partie de path allant jusqu'à son dernier os.sep
+# -sys.path.append() permet d'ajouter
+sys.path.append(path[:path.rfind(os.sep)])
+import beaglebot
+
+class Bot(beaglebot.BeagleBot):
+
+    def process_candle(self, candle_msg):
+        super().process_candle(candle_msg)
